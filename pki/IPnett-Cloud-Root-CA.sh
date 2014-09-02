da61222e2503a362dac9b2531021002d4e83f946
Jakob Schlyter <jakob@kirei.se>
2014-09-02 11:51:44 +0200

    init

---
+++ pki/IPnett-Cloud-Root-CA.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+PASSWORD=mekmitasdigoat
+
+KDB=/opt/tivoli/tsm/client/ba/bin/dsmcert.kdb
+GSK8CAPICMD=/opt/tivoli/tsm/client/ba/bin/gsk8capicmd_64
+
+$GSK8CAPICMD -keydb -create -db $KDB -pw $PASSWORD -stash
+
+$GSK8CAPICMD -cert -add -db $KDB \
+	-label "IPnett BaaS Root CA" \
+	-file IPnett-Cloud-Root-CA.pem \
+	-format ascii -stashed
+
+$GSK8CAPICMD -cert -list -db $KDB -stashed
