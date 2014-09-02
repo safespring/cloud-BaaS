0a1f5eca33a95db10b0beb31ee69f3b55bc7b9e6
Jakob Schlyter <jakob@kirei.se>
2014-09-02 14:12:51 +0200

    compact

--- pki/IPnett-Cloud-Root-CA.sh
+++ pki/IPnett-Cloud-Root-CA.sh
@@ -7,9 +7,8 @@ GSK8CAPICMD=gsk8capicmd_64
 
 $GSK8CAPICMD -keydb -create -db $KDB -pw $PASSWORD -stash
 
-$GSK8CAPICMD -cert -add -db $KDB \
+$GSK8CAPICMD -cert -add -db $KDB -format ascii -stashed \
 	-label "IPnett BaaS Root CA" \
 	-file IPnett-Cloud-Root-CA.pem \
-	-format ascii -stashed
 
 $GSK8CAPICMD -cert -list -db $KDB -stashed
