6d83f6b344293bbfc5e1ce777a75ee27af873e26
Janne Johansson <janne.johansson@ipnett.com>
2014-09-10 09:34:12 +0200

    Add quotes around variables, in case they have spaces or other chars

--- pki/IPnett-Cloud-Root-CA.sh
+++ pki/IPnett-Cloud-Root-CA.sh
@@ -6,10 +6,10 @@ PASSWORD=MoghooNaeFie5sipi9aegheixiNg5che
 KDB=/opt/tivoli/tsm/client/ba/bin/dsmcert.kdb
 GSK8CAPICMD=gsk8capicmd_64
 
-$GSK8CAPICMD -keydb -create -db $KDB -pw $PASSWORD -stash
+$GSK8CAPICMD -keydb -create -db "$KDB" -pw "$PASSWORD" -stash
 
-$GSK8CAPICMD -cert -add -db $KDB -format ascii -stashed \
+$GSK8CAPICMD -cert -add -db "$KDB" -format ascii -stashed \
 	-label "IPnett BaaS Root CA" \
 	-file IPnett-Cloud-Root-CA.pem \
 
-$GSK8CAPICMD -cert -list -db $KDB -stashed
+$GSK8CAPICMD -cert -list -db "$KDB" -stashed
