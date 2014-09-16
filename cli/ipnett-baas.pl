1628e0b7fbd9e263b3f6cc3e3a621033d3c35f33
Jakob Schlyter <jakob@kirei.se>
2014-09-16 14:22:24 +0200

    better docs

--- cli/ipnett-baas.pl
+++ cli/ipnett-baas.pl
@@ -370,4 +370,15 @@ ipnett-baas [options] [command]
 B<ipnett-baas> is a simple CLI for the IPnett BaaS REST API. Default
 configuration file is ipnett-baas.yaml in the current working directory.
 
+=head1 CONFIGURATION
+
+B<ipnett-baas> reads its default configuration, in YAML format, from the file
+B<ipnett-baas.yml> in the current working directory. An example configuration
+file can be found below:
+
+    endpoint: https://api.cloud.ipnett.se/tsm/
+    access_key_id: xaivookaelei
+    secret_access_key: shooJoo6eePiPhaesh1nengi
+    verify_hostname: 1
+
 =cut
