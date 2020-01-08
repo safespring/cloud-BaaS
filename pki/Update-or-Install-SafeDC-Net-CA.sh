7c759f3fd53df2d492bb1cc2a322d793827ba947
Janne Johansson <janne.johansson@safespring.com>
2020-01-08 14:41:12 +0100

    remove spaces and add test for pem file

--- pki/Update-or-Install-SafeDC-Net-CA.sh
+++ pki/Update-or-Install-SafeDC-Net-CA.sh
@@ -3,8 +3,15 @@
 KDB=/opt/tivoli/tsm/client/ba/bin/dsmcert.kdb
 GSK8CAPICMD=gsk8capicmd_64
 
-$GSK8CAPICMD -cert -add -db "$KDB" -format ascii -stashed \
-	-label "SafeDC Net Root CA" \
-	-file SafeDC-Net-Root-CA.pem
+if [ -f SafeDC-Net-Root-CA.pem ]; then
 
-$GSK8CAPICMD -cert -list -db "$KDB" -stashed
+    $GSK8CAPICMD -cert -add -db "$KDB" -format ascii -stashed \
+		 -label "SafeDC Net Root CA" \
+		 -file SafeDC-Net-Root-CA.pem
+
+    $GSK8CAPICMD -cert -list -db "$KDB" -stashed
+else
+    echo "SafeDC cert file SafeDC-Net-Root-CA.pem missing,"
+    echo "please place it in current dir and re-run script"
+    exit 1
+fi
