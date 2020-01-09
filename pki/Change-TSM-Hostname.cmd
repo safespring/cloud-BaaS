2dac6e0608289458fa7225d617bba39618b04d6a
Janne Johansson <janne.johansson@safespring.com>
2020-01-09 12:35:56 +0100

    Remove wrong bin path in previous version

--- pki/Change-TSM-Hostname.cmd
+++ pki/Change-TSM-Hostname.cmd
@@ -1,5 +1,5 @@
 
-cd "C:\Program Files\tivoli\tsm\baclient\bin\"
+cd "C:\Program Files\tivoli\tsm\baclient\"
 
 powershell -Command "(gc dsm.opt) -replace 'TCPSERVERADDRESS.*tsm1.cloud.ipnett.se', 'TCPSERVERADDRESS tsm1.backup.sto2.safedc.net' | Out-File -encoding ASCII dsm.opt.edited"
 
