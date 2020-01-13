5057c532812d932372f5bbb9a77ceac1fd4933d5
Janne Johansson <icepic.dz@gmail.com>
2020-01-13 13:55:44 +0100

    Update Change-TSM-Hostname.cmd

--- pki/Change-TSM-Hostname.cmd
+++ pki/Change-TSM-Hostname.cmd
@@ -1,5 +1,6 @@
 
-cd "C:\Program Files\tivoli\tsm\baclient\"
+@cd "C:\Program Files\tivoli\tsm\baclient\"
 
 powershell -Command "(gc dsm.opt) -replace 'TCPSERVERADDRESS.*tsm1.cloud.ipnett.se', 'TCPSERVERADDRESS tsm1.backup.sto2.safedc.net' | Out-File -encoding ASCII dsm.opt.edited"
-
+@echo "Please verify changes to C:\Program Files\tivoli\tsm\baclient\dsm.opt.edited"
+@echo "looks ok and copy it over dsm.opt and restart TSM services"
